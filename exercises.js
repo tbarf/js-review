609ad12f5d98194f3587bbaa1d01b1a59e60e0c4
tbarf <30032737+tbarf@users.noreply.github.com>
2017-09-07 13:24:29 -1000

    done up to 18

--- exercises.js
+++ exercises.js
@@ -20,11 +20,29 @@ console.log(yourName);
 Declare variables named numberOfFloorsBurjKhalifa, TokyoOlympicsYear. Please assign the actual data for each variable.
 Console log each variable.*/
 
+var numberOfFloorsBurjKhalifa = 163;
+console.log(numberOfFloorsBurjKhalifa);
+var TokyoOlympicsYear = 1988;
+console.log(TokyoOlympicsYear);
+
+
 /*3. Variables with Boolean values
 Declare variables named lovesPrepClass, eatsDonuts, drinksGin.  Console log each variable.*/
+var lovesPrepClass = true;
+console.log(lovesPrepClass);
+var eatsDonuts  = true;
+console.log(eatsDonuts);
+var drinksGin = false;
+console.log(drinksGin);
+
 
 /*4. Variable with Null values
 Declare variables named completedCodingChallenge, traveledToSpace.  Console log each variable*/
+var completedCodingChallenge = null;
+console.log(completedCodingChallenge);
+var traveledToSpace = null;
+console.log(traveledToSpace);
+
 
 /*5.  Variables with Array for its value.
 Declare a variable named japanPrefectures and assign the value of any 5 prefectures in Japan.
@@ -32,34 +50,65 @@ Console log the variable
 Console log the value at index 1.
 Console log the value at index 3.*/
 
+var japanPrefectures = ["Kyoto", "Nara", "Saga", "Tochigi", "Osaka"];
+console.log(japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
+
+
+
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.
 Console log the variable
 Console log "Earth" using the index position
 Console log "Uranus" using the index position*/
 
+var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Nepture", "Pluto"];
+console.log(planets);
+console.log(planets[2]);
+console.log(planets[6]);
+
 /*7. Variables with Array for its value.
 Declare a variable named india and assign the value of 3 popular tourist attractions.
 Console log the variable.*/
+
+var india = ["Taj Mahal", "Amer Fort", "The Red Fort"];
+console.log(india);
 
 /*8. Adding new elements to an array.
 Add another popular tourist attraction to the india variable from position 1 of the index.
 (hint, it is not the push() method). Console log the variable.*/
 
+india.splice(1,0,"Hawa Mahal");
+console.log(india);
+
+
 /*9. Removing the last element from an array.
 Using the same india variable, remove the last element from the array. Console log the variable. */
 
+india.splice(3,1);
+console.log(india);
+
 /*10. Removing an element from an array into a new array.
-Removethe first two elements in the snacks array and return a new array.
+Remove the first two elements in the snacks array and return a new array.
 Console log the new variable. */
 
 var snacks = ["peanut butter cup oreo", "birthday cake oreo", "banana split oreo", "watermelon oreo", "cookie dough oreo", "creamsicle oreo"];
+
+var arry = snacks.slice(0,2);
+console.log(arry);
 
 /*11. Declare two variables with names of your choosing and use the triple comparison operator (===) to compare:
 a) two strings "Racecar" and "racecar"
 b) a string and a number "three" and 3
 
 console log each variable*/
+
+var car = "Racecar" === "racecar";
+console.log(car);
+var num = "three" === 3;
+console.log(num);
+
 
 /*12.  Declare a literal object named "hotel" with properties and values set to:
 
@@ -70,6 +119,15 @@ console log each variable*/
 
  console log the object*/
 
+var hotel =
+    {
+  name: "Hilton",
+  rooms: 200,
+  pool:  true,
+  checkIn: null
+};
+console.log(hotel);
+
 /*13.  Declare a literal object named "car" with properties and values set to:
 
     make  => "BMW"
@@ -79,6 +137,17 @@ console log each variable*/
 
  console log the object*/
 
+var car =
+    {
+      make: "BMW",
+      model: "X5",
+      year: 2015,
+      doors: 4
+    };
+console.log(car);
+
+
+
 /*14.  Declare a literal object named bigBox and set its value to be an empty object.
 
 Add a property named size and set it to a random number value between 0 and 50 (hint, use Math.something...)
@@ -86,6 +155,16 @@ Add a property named color and set it to any color.
 Add a property named contents and set it to be an empty array
 
 console log the object*/
+
+var bigBox =
+
+    {
+    size: Math.random(0,50) * (50 - 0) + 0,
+      color: "purple",
+      contents: []
+    };
+console.log(bigBox);
+
 
 /*15.  Declare a literal object named "colorMeBadd" with properties and values set to:
 
@@ -102,9 +181,39 @@ console log the object*/
 
 console log the object */
 
+colorMeBadd =
+  {
+  akai: "red",
+  aoi:  "blue",
+  midori: "green",
+  shiroi: "white",
+  pinku:  "pink",
+  murasaki: "purple",
+  isJapanese: true
+};
+console.log(colorMeBadd);
+
+
 /*16. As the general manager for the DevLeague Prep Beer Pong Team, your job is to select the starting players from the roster array. Create two variables, Ateam and Bteam and assign each an empty array. You will then assigned players that have an even number index position to the Ateam and odd number index position to the Bteam. Let the games begin! Console log the results.*/
 
 var roster = ["Alex", "Laura", "Sami", "Jasmine", "Adam", "Dakota", "Jonathon", "Luther", "Kevin", "Vic"];
+
+var newTeams = {
+Ateam:  [],
+Bteam:  []
+};
+
+
+
+  for (var i = 0; i < roster.length; i++){
+  if(i % 2 === 0){
+    newTeams.Ateam.push(roster[i]);
+  } else {
+    newTeams.Bteam.push(roster[i]);
+  }
+  }
+
+console.log(newTeams);
 
 /*17. Using the japanPrefectures array you created in exercise 5, create a for loop that iterates through this array and console logs the following message:
 
@@ -112,9 +221,24 @@ var roster = ["Alex", "Laura", "Sami", "Jasmine", "Adam", "Dakota", "Jonathon", 
 
 i.e. "The locaton at 0 is Tokyo."*/
 
+
+for (var i = 0; i<japanPrefectures.length; i++){
+console.log("The value at " + i + " is: " + japanPrefectures[i]);
+}
+
 /*18. Declare a function named addUp.
 This function will take a number parameter and add up all the numbers from 1 to that number.
 The parameter will be any number from 1 to 88.*/
+function addUp(num){
+var sum = 0;
+ for (var i =1; i <=num; i++){
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(addUp(2));
+
 
 /*19. Declare a variable named oddNumbers and assign it to an empty array
 Use a for loop to add only odd numbers to an Array.  Add 30 odd numbers to the oddNumbers variable starting with the value of 1.*/
